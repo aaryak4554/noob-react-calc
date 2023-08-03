94150a230ddde2daba5e130fdc32b89d27ce4272
aaryak4554 <joinaarya30@gmail.com>
2023-08-03 19:19:02 +0530

    basic struct

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import './App.css';
+import { useState } from 'react';
+import './style.css';
 
 function App() {
+    const [count, setCount]=useState(0);
+    const [a, setA]=useState();
+    const [b, setB]=useState();
+    const [sign, setSign]=useState("+");
+    const [result, setResult]=useState(" ");
+    function handleAdd(){
+        setResult(a+b);
+    }
+    function handleReset(){
+        setA(" ");
+        setB(" ");
+        setSign("+");
+    }
   return (
-    <div className="App">
-      <h1>hiiii thereeeee</h1>
+    <div className="main">
+      <p>hiiii thereeeee</p>
+      <p>Total operations: 0</p>
+      <div className='sub'>
+            <input 
+                className='num'
+                type='text'
+                placeholder='number 1...'
+                value={a}
+                />
+            <div className='sign'>{sign}</div>
+            <input 
+                className='num'
+                type='text'
+                placeholder='number 1...'
+                value={b}
+                />
+      </div>
+      <div className='ops'>
+        <button onClick={handleAdd}>+</button>
+        <button>-</button>
+        <button>*</button>
+        <button>/</button>
+      </div>
+      <p>Result: {result}</p>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 }
