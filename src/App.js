6b0c5d6494de0369f8604fc8b97dc7af74bd07b9
aaryak4554 <joinaarya30@gmail.com>
2023-08-05 12:02:28 +0530

    added css

--- src/App.js
+++ src/App.js
@@ -44,33 +44,33 @@ function App() {
 
     return (
         <div className="main">
-        <p>hiiii thereeeee</p>
-        <p>Total operations: {count}</p>
-        <div className='sub'>
-                <input 
-                    className='num'
-                    type='number'
-                    placeholder='number 1...'
-                    value={a}
-                    onChange={handleChangeA}
-                />
-                <div className='sign'>{sign}</div>
-                <input 
-                    className='num'
-                    type='number'
-                    placeholder='number 2...'
-                    value={b}
-                    onChange={handleChangeB}
-                />
-        </div>
-        <div className='ops'>
-            <button onClick={handleAdd}>+</button>
-            <button onClick={handleSubtract}>-</button>
-            <button onClick={handleMultiply}>*</button>
-            <button onClick={handleDivide}>/</button>
-        </div>
-        <p>Result:{result}</p>
-        <button onClick={handleReset}>Reset</button>
+            <p className='heading1'>noob-react-calc</p>
+            <p className='heading2'>Total Operations: {count}</p>
+            <div className='sub'>
+                    <input 
+                        className='num'
+                        type='number'
+                        placeholder='number 1...'
+                        value={a}
+                        onChange={handleChangeA}
+                    />
+                    <div className='ops-sign'>{sign}</div>
+                    <input 
+                        className='num'
+                        type='number'
+                        placeholder='number 2...'
+                        value={b}
+                        onChange={handleChangeB}
+                    />
+            </div>
+            <div className='ops'>
+                <button className='signbtn' onClick={handleAdd}>+</button>
+                <button className='signbtn' onClick={handleSubtract}>-</button>
+                <button className='signbtn' onClick={handleMultiply}>*</button>
+                <button className='signbtn' onClick={handleDivide}>/</button>
+            </div>
+            <p className='result'>Result: {result}</p>
+            <button className='resetbtn' onClick={handleReset}>Reset</button>
         </div>
     );
 }
